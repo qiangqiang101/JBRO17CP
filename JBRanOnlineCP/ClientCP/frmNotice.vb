b01b3deac1f68f1f35cadd7da62207799ed22c16
Office <qiangqiang101@hotmail.com>
2017-07-29 11:47:16 +0800

    cleaning

--- JBRanOnlineCP/ClientCP/frmNotice.vb
+++
@@ -1,5 +0,0 @@
-﻿Public Class frmNotice
-    Private Sub frmNotice_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        WebBrowser1.Navigate(ReadCfgValue("cpNews", My.Application.Info.DirectoryPath & "\param.cfg"))
-    End Sub
-End Class
